1d97a06938ef93a03f58180e5fbaa1b0a92358bb
miqt <miqtdev@163.com>
2021-01-13 17:12:13 +0800

    'init'

---
+++ plugin/method_hook/src/main/java/com/miqt/asm/method_hook/MyPlugin.java
@@ -0,0 +1,12 @@
+package com.miqt.asm.method_hook;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MyPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
